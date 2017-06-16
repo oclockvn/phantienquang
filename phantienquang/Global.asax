c8397df185040301302ab4c92eb48f5a04564bae
oclockvn <oclockvn@gmail.com>
2017-06-16 09:41:56 +0700

    Add project files.

---
+++ phantienquang/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="phantienquang.MvcApplication" Language="C#" %>
---
+++ phantienquang/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(phantienquang.Startup))]
+namespace phantienquang
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
